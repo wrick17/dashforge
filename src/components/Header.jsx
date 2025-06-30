f543fa541a0fa49300fdb4b400272d63e5e953e4
Pratyush Poddar <pratyush.poddar@sequoia.com>
2025-06-30 11:26:59 +0530

    lock unlock fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,29 @@
-import { Check, Pencil } from "lucide-react";
-import { useEditable } from "../hooks/store";
+import { Lock, LockOpen } from 'lucide-react';
+import { useEditable } from '../hooks/store';
 import { Tabs } from './Tabs';
 
 export const Header = () => {
 	const { isEditing, startEditing, stopEditing } = useEditable();
+
+	const handleLocking = () => {
+		if (isEditing) {
+			stopEditing();
+		} else {
+			startEditing();
+		}
+	};
+
 	return (
 		<header className="flex flex-row justify-between items-center p-2 gap-4">
 			<span className="text-2xl font-bold px-2">DashForge</span>
 			<Tabs />
 			<button
 				type="button"
-				className="rounded-full bg-gray-700 text-white px-4 py-2 flex flex-row items-center justify-center gap-2 min-w-24"
-				onClick={() => {
-					if (isEditing) {
-						stopEditing();
-					} else {
-						startEditing();
-					}
-				}}
+				className="rounded-full bg-gray-700 text-white px-2 py-2 flex flex-row items-center justify-center gap-2"
+				title={isEditing ? 'Lock' : 'Unlock'}
+				onClick={handleLocking}
 			>
-				{isEditing ? <Check size={18} /> : <Pencil size={16} />}
-				{isEditing ? "Save" : "Edit"}
+				{isEditing ? <Lock size={18} /> : <LockOpen size={18} />}
 			</button>
 		</header>
 	);
